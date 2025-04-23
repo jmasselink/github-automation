8d353fb75e1894dbce28323524579ce134f76ea8
Joel Masselink <jmasselink@gmail.com>
2025-04-23 10:11:53 -0400

    Update run.sh
    download airports

--- run.sh
+++ run.sh
@@ -1,1 +1,1 @@
-curl "https://hub.arcgis.com/api/download/v1/items/cd2da0109c1b46c3a2ec9890e44623d3/csv?redirect=false&layers=1" --output data.csv
+curl "https://davidmegginson.github.io/ourairports-data/airports.csv" --output data.csv
